2d94b538d30df2523de9126b5f4ecf5249d097d5
Barbra Wasiwo <b.wasiwo.bw@gmail.com>
2023-04-27 16:14:20 -0700

    Print numners

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A program that prints the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha = 'a'
+		while (alpha <= 'z')
+
+			putchar(alpha);
+			alpha++;
+				putchar(\n);
+}
+return (0);
+}
