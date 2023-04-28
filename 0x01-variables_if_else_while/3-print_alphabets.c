ec457a94d0d53c495b4d5e5134a71eb362f65bb0
Barbra Wasiwo <b.wasiwo.bw@gmail.com>
2023-04-28 00:08:12 -0700

    Upper-lower

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints uppercase and lowercase letters
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+
+		putchar(alpha);
+
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+
+		putchar(alpha);
+
+	putchar('\n');
+
+	return (0);
+}
+
