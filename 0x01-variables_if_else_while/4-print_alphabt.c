8c232d3df01e4450ce7526bc4da8f5e3dde91b03
Barbra Wasiwo <b.wasiwo.bw@gmail.com>
2023-04-27 23:57:49 -0700

    Alphabet print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,26 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+
 /**
  * main - Entry point
+ *
  * Description: A program that prints the alphabet in lowercase
+ *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char alpha;
-		for (alpha = 'a'; alpha <= 'z'; alpha++)
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha != 'e' && alpha != 'q')
 
 			putchar(alpha);
+	}
+	putchar('\n');
 
-				putchar('\n');
-
-				return (0);
+	return (0);
 }
