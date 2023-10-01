02444b670822d22aaa72d95b3c428c0e7fdbf232
Barbra Wasiwo <b.wasiwo.bw@gmail.com>
2023-10-01 00:28:09 -0700

    Locked away

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,6 +4,7 @@
  * _puts_recursion - Entry point
  * Description: 'a function that prints a string, followed by a new line'
  * @s: the char string type
+ * _putchar - prints the character c to stdout
  * Return: void
  */
 void _puts_recursion(char *s)
@@ -11,7 +12,7 @@ void _puts_recursion(char *s)
 	if (*s)
 	{
 		_putchar(*s);
-		_puts_recursion(s+ 1);
+		_puts_recursion(s + 1);
 	}
 	else
 	{
