454e5619ff065c98d59c81d7c9e9242d6de4e89f
Barbra Wasiwo <b.wasiwo.bw@gmail.com>
2023-10-09 02:49:35 -0700

    Realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,19 +17,21 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr != NULL)
 		dupli = ptr;
 	else
-	{return(malloc(new_size)); }
+	{return (malloc(new_size)); }
 
 	if (new_size == old_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
 	{free(ptr);
+		return (0); }
+	relloc = malloc(new_size);
+	if (relloc == NULL)
 		return (0);
-
-		for (h = 0; h < (old_size || h < new_size); h++)
+	
+	for (h = 0; h < (old_size || h < new_size); h++)
 		{
 			*(relloc + h) = dupli[h];
 		}
 		free(ptr);
 		return (relloc);
-	}
 }
